4148b76ae2b14a7f8fca0cdbb60398a7a7bf5aaf
jonassvensson <jonas.svensson@midsummer.se>
2025-05-14 15:33:51 +0200

    misc: added a e2e test

--- e2e-tests/firsttest.spec.js
+++ e2e-tests/firsttest.spec.js
@@ -7,4 +7,9 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+  test('can navigate to a particular Pokemon', async ({ page }) => {
+    await page.goto('')
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('ivysaur')).toBeVisible()
+  })
 })
