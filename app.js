c8456b530c4c2de1654605c8222495fda158d37f
jonassvensson <jonas.svensson@midsummer.se>
2025-05-15 10:16:10 +0200

    misc: reverted back to working health check

--- app.js
+++ app.js
@@ -9,14 +9,21 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('3') // change this string to ensure a new version deployed
+  res.send('4') // change this string to ensure a new version deployed
 })
 
+// Render is set up to check this endpoint for periodic health checks.
 app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+// Simulate a broken deployment
+/* app.get('/health', (req, res) => {
   // eslint-disable-next-line no-constant-condition
   if (true) throw('error...  ')
   res.send('ok')
 })
+ */
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
