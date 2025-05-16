7066ad9a2d47ca9d3261bd588b91321767bf69f6
Jonas Svensson <jonas.svensson@midsummer.se>
2025-05-16 11:30:51 +0200

    feat: updated version

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('14') // change this string to ensure a new version deployed
+  res.send('15') // change this string to ensure a new version deployed
 })
 
 // Render is set up to check this endpoint for periodic health checks.
